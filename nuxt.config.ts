c1e74e74d7e062ab8bab24ed7f012394529a03b3
TigRig <tigrig.crex@gmail.com>
2020-02-21 20:25:55 +0900

    Add: dotenv モジュールおよび .env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,8 @@ const config: Configuration = {
   buildModules: [
     '@nuxtjs/eslint-module',
     '@nuxt/typescript-build',
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/dotenv'
   ],
   modules: ['@nuxtjs/pwa', '@nuxtjs/style-resources', 'nuxt-fontawesome'],
   styleResources: {
@@ -32,6 +33,12 @@ const config: Configuration = {
   },
   fontawesome: {
     component: 'fa'
+  },
+  env: {
+    // contentful
+    CTF_SPACE_ID: process.env.CTF_SPACE_ID as string,
+    CTF_BLOG_POST_TYPE_ID: process.env.CTF_BLOG_POST_TYPE_ID as string,
+    CTF_CDA_ACCESS_TOKEN: process.env.CTF_CDA_ACCESS_TOKEN as string
   },
   build: {
     babel: {
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,46 @@
 <template>
-  <div class="Block">
-    <section-sample />
+  <div class="container">
+    <div v-if="posts.length">
+      <ul v-for="(post, i) in posts" :key="i">
+        <li>{{ post.fields.title }}</li>
+        <ul>
+          <img
+            v-if="post.fields.image !== undefined"
+            :src="post.fields.image.fields.file.url"
+            :alt="post.fields.image.fields.title"
+            :aspect-ratio="16 / 9"
+            max-width="400"
+            max-height="225"
+          />
+          <li>{{ post.fields.body }}</li>
+          <li>{{ post.fields.publishedDate }}</li>
+        </ul>
+      </ul>
+    </div>
+    <div v-else>
+      投稿された記事はありません。
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'nuxt-property-decorator'
 
-import sectionSample from '~/components/sections/Samples.vue'
+import client from '~/plugins/contentful'
 
-@Component({
-  components: {
-    sectionSample
+@Component
+export default class Index extends Vue {
+  async asyncData({ env }) {
+    let posts: any = []
+    await client
+      .getEntries({
+        content_type: env.CTF_BLOG_POST_TYPE_ID,
+        order: '-fields.publishedDate'
+      })
+      .then((res) => (posts = res.items))
+      .catch(console.error)
+    console.log(posts)
+    return { posts }
   }
-})
-export default class Index extends Vue {}
+}
 </script>
